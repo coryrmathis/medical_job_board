8d5a5b7178be5a8d9d4020d103fa305e0d395cdb
Cory Mathis <coryrmathis@gmail.com>
2017-09-27 10:37:18 -0700

    Add 404 catching for job shit.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,9 @@ class JobsController < ApplicationController
   def show
     # actual_id = params[:id] / 10
     response = RestClient.get("https://archwaycrm.herokuapp.com/api/positions/#{params[:id]}")
+  rescue RestClient::NotFound => e 
+    render 'errors/job_unavailable'
+  else
     @job = Job.new(JSON.parse(response.body))
 
   end
