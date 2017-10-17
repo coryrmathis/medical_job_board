3b725044efce3b268a6a5d4ed7ebec0d25f66d56
Cory Mathis <coryrmathis@gmail.com>
2017-10-16 17:14:24 -0700

    Working toward a fully functional update process sourced from CRM s3 bucket.

--- app/jobs/synchronize_positions_job.rb
+++ app/jobs/synchronize_positions_job.rb
@@ -1,4 +1,4 @@
-class GetActiveJobsJob
+class SynchronizePositionsJob
   include Sidekiq::Worker
 
   attr_reader :s3, :object 
@@ -9,12 +9,30 @@ class GetActiveJobsJob
   end
 
   def perform
-    download_import_file
+    file = download_import_file
+    synchronize(file)
+  end
+
+  def synchronize(file)
+    options = {
+      chunk_size: 1000,
+      force_utf8: true,
+      invalid_byte_sequence: ""
+    }
+
+    SmarterCSV.process(file, options) do |chunk|
+      chunk.each do |job|
+        # Any Job that isn't present in the list but is in the db must be detected and deleted
+        # Find Job by aid/id
+        # Update if found/Create if not
+      end
+    end
   end
 
   def download_import_file
     FileUtils.mkdir_p(path)
     s3.get_object({ bucket: ENV["ARCHWAY_S3_BUCKET"], key: object }, target: path.join("active_jobs.csv"))
+    path.join("active_jobs.csv")
   end
 
   def path
