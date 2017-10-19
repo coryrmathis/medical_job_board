4f2bc14a500a98973082f38cf390f482f227fdaf
Cory Mathis <coryrmathis@gmail.com>
2017-10-19 13:19:22 -0700

    Jobs now sourced locally instead of through CRM api.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,10 @@
 class Job < ApplicationRecord
 
   scope :archway, ->{where.not(aid: nil)}
+
+  def job_description_markup
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer)
+    markdown.render(job_description).html_safe
+  end
 end
