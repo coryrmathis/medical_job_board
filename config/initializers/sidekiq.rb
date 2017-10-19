6d04169279f498c9001abc29c4d5bccffbbec57f
Cory Mathis <coryrmathis@gmail.com>
2017-10-19 13:00:40 -0700

    Configure number of sidekiq clients as 2.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 2 }
+end
+
 Sidekiq.configure_server do |config|
   schedule_file = "config/schedule.yml"
   if File.exists?(schedule_file)
