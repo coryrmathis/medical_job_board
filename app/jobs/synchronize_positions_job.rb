a24d72ca551f779ec9103aa7baea702e67385555
Cory Mathis <coryrmathis@gmail.com>
2017-10-19 10:26:27 -0700

    SynchronizePositionsJob is functional. Now need to schedule it.

--- app/jobs/synchronize_positions_job.rb
+++ app/jobs/synchronize_positions_job.rb
@@ -1,11 +1,12 @@
 class SynchronizePositionsJob
   include Sidekiq::Worker
 
-  attr_reader :s3, :object 
+  attr_reader :s3, :object, :import_ids 
 
   def initialize
     @s3 = configure_s3
     @object = "job_board/active_positions.csv"
+    @import_ids = []
   end
 
   def perform
@@ -14,29 +15,23 @@ class SynchronizePositionsJob
   end
 
   def synchronize(file)
-    options = {
-      chunk_size: 1000,
-      force_utf8: true,
-      invalid_byte_sequence: ""
-    }
-    # Set up container to collect ids being import (current active jobs)
-    import_ids = []
-
-    SmarterCSV.process(file, options) do |chunk|
+    SmarterCSV.process(file, smarter_csv_options) do |chunk|
       chunk.each do |position_data|
-        # Any Job that isn't present in the list but is in the db must be detected and deleted
-        # Find Job by aid/id
-        # Update if found/Create if not
         import_ids << position_data[:id]
         JobImporter.new(position_data).import
       end
     end
 
+    destroy_inactive_jobs
+  end
+
+  def destroy_inactive_jobs
     # Generate an array of the aids of archway jobs currenty in database
     archway_ids = Job.archway.pluck(:aid)
     # Filter out ids of jobs which were present in the import list
-    delete_ids = archway_ids.reject{|id| import_ids.include?(id)}
+    to_delete_ids = archway_ids.reject{|id| import_ids.include?(id)}
     # Delete the jobs which weren't present in the import list (They're now inactive in the CRM)
+    Job.where(aid: to_delete_ids).destroy_all
   end
 
   def download_import_file
@@ -50,6 +45,14 @@ class SynchronizePositionsJob
   end
 
   private
+
+  def smarter_csv_options
+    {
+      chunk_size: 1000,
+      force_utf8: true,
+      invalid_byte_sequence: ""
+    }
+  end
 
   def configure_s3
     Aws.config.update({
