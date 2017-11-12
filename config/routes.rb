80546126f6108eedb439cee13d78df452418fd42
Cory Mathis <coryrmathis@gmail.com>
2017-11-12 11:27:11 -0800

    Create Applicant model.  Add devise. Generate devise views and controllers. Working on application/registration form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 require 'sidekiq/cron/web'
 Rails.application.routes.draw do
+  devise_for :applicants, controllers: {registrations: "applicants/registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
 
