2e1c4364cd6d7fff76bd2ab37f04cbfbe0b1450e
Cory Mathis <coryrmathis@gmail.com>
2018-01-04 11:17:39 -0600

    Contact name included as column on Jobs model. Notice given to applicant upon successful creation of new application.

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,5 +2,7 @@ class Application < ApplicationRecord
   belongs_to :job
   mount_uploader :cv, CVUploader
 
-  
+  def new_application_notice
+    "Thank you for your application! #{job.contact_name} (#{job.contact_email}) with Archway Physician Recruitment, or another recruiter with this firm, will be reaching out to you via email regarding your application."
+  end
 end
