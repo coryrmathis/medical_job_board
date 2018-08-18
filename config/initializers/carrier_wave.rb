b5c72bb3a56f498c0376af9d8ea66294782aa21f
Glen Lindquist <glenlindquist@gmail.com>
2018-08-18 13:14:06 -0500

    Temporarily turn off initializers

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['S3_ACCESS_KEY'],                        # required
-    aws_secret_access_key: ENV['S3_SECRET_KEY'],                        # required
-    region:                ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
+    # provider:              'AWS',                        # required
+    # aws_access_key_id:     ENV['S3_ACCESS_KEY'],                        # required
+    # aws_secret_access_key: ENV['S3_SECRET_KEY'],                        # required
+    # region:                ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
