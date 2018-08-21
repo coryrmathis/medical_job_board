a0d59b338cf1d85fcd019bcb2e8c7df82da301be
Glen Lindquist <glenlindquist@gmail.com>
2018-08-21 14:27:39 -0500

    Fix up some devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 require 'sidekiq/web'
 require 'sidekiq/cron/web'
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
 
