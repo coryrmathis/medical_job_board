4d64f57eec2cd2cb5b30ac85971acaccc249f795
Glen Lindquist <glenlindquist@gmail.com>
2018-09-26 15:03:04 -0500

    Adjust rails data exposed to react app

--- app/helpers/react_browser/react_helper.rb
+++ app/helpers/react_browser/react_helper.rb
@@ -3,7 +3,8 @@ module ReactBrowser::ReactHelper
   def rails_data
     {
       specialties: Specialty.pluck(:name).sort,
-      states: CS.states(:us)
+      states: CS.states(:us),
+      userAccountType: current_user.account_type,
     }
 
   end
