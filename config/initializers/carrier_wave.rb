6e036c39ba3ffebd34479f6230aa28820b6baf67
Glen Lindquist <glenlindquist@gmail.com>
2019-01-14 16:36:06 -0600

    Remove optional carrier wave lines

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV['S3_ACCESS_KEY'],                        # required
     aws_secret_access_key: ENV['S3_SECRET_KEY'],                        # required
     region:                ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
-    host:                  's3.example.com',             # optional, defaults to nil
-    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    # host:                  's3.example.com',             # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = ENV['S3_BUCKET']                         # required
   config.fog_public     = false                                        # optional, defaults to true
